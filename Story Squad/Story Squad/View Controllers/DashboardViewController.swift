aee4773d3f8b2eddf23b4ae531ab654aa9cbf24f
Wilma <kima_92@fastmail.com>
2020-02-20 19:06:44 -0500

    Fixed pumpkin stroke color on Storyboard label

--- Story Squad/Story Squad/View Controllers/DashboardViewController.swift
+++ Story Squad/Story Squad/View Controllers/DashboardViewController.swift
@@ -10,26 +10,30 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
-    override func viewDidLoad() {
-        updateViews()
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
     @IBOutlet weak var storySquadLabel: UILabel!
     
-    func updateViews() {
-        storySquadLabel.textColor.setStroke()
-        setStrokePumpkin()
-    }
-
-    func setStrokePumpkin() {
-        pumpkinColor.setStroke()
+    let sqLabelStrokeAttributes: [NSAttributedString.Key: Any] = [
+//        .foregroundColor: UIColor(red: 0, green: 0.477, blue: 0.733, alpha: 1),
+//        .font: UIFont(name: "Avenir", size: 100)!,
+        .strokeColor: UIColor(red: 1, green: 0.427, blue: 0.227, alpha: 1),
+        .strokeWidth: -3.5
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+        
     }
     
-    //Red: 0, green: 0.447, blue: 0.733, alpha: 1)
-    let pumpkinColor = UIColor(displayP3Red: 1, green: 0.427, blue: 0.227, alpha: 1)
+    func updateViews() {
+        
+        let pumkinStrokeAttribute = NSAttributedString(string: storySquadLabel.text!, attributes: sqLabelStrokeAttributes)
+        
+        storySquadLabel.attributedText = pumkinStrokeAttribute
+        storySquadLabel.textColor = UIColor(red: 0, green: 0.477, blue: 0.733, alpha: 1)
+    }
+
     
     /*
     // MARK: - Navigation
