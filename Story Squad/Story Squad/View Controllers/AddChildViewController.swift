81d46edd68b981927751d02b825a3f5fd4bf2339
jonalynn masters <jonalynna.masters@gmail.com>
2020-02-25 13:13:24 -0800

    Organized files by name; segues WIP

---
+++ Story Squad/Story Squad/View Controllers/AddChildViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddChildViewController.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 2/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddChildViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
