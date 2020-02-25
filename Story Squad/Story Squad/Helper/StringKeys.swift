47d4ddd9cc212d9bde1a140361090db65ccc5c17
Percival N <percival.ngan@gmail.com>
2020-02-25 14:20:09 -0600

    Started segue work

---
+++ Story Squad/Story Squad/Helper/StringKeys.swift
@@ -0,0 +1,14 @@
+//
+//  StringKeys.swift
+//  Story Squad
+//
+//  Created by Percy Ngan on 2/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+	static var addChildSegue = "AddChild"
+}
