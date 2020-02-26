6c43864861254f398c3c1962b478caf8bfc2fa36
Percival N <percival.ngan@gmail.com>
2020-02-26 10:53:32 -0600

    Added blank convenience and representation files

---
+++ Story Squad/Story Squad/Models/Parent+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Parent+Convenience.swift
+//  Story Squad
+//
+//  Created by Percy Ngan on 2/26/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Story Squad/Story Squad/Models/ParentRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  ParentRepresentation.swift
+//  Story Squad
+//
+//  Created by Percy Ngan on 2/26/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
