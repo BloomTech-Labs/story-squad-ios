f851494c9986a2d3e29e376871ea0a773d2273e0
Percival N <percival.ngan@gmail.com>
2020-02-26 13:51:22 -0600

    Added outlets to ParentInfoVC and implemented done button.

--- Story Squad/Story Squad/View Controllers/ParentInfoViewController.swift
+++ Story Squad/Story Squad/View Controllers/ParentInfoViewController.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class ParentInfoViewController: UIViewController {
 
+
+	// MARK: - Outlets
+	@IBOutlet weak var nameTextField: UITextField!
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	@IBOutlet weak var confirmPWTextField: UITextField!
+	@IBOutlet weak var pinTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+
+	// MARK: - Actions
+	@IBAction func doneButton(_ sender: Any) {
+
+		guard let name = nameTextField.text,
+			!name.isEmpty,
+			let emailTextField = emailTextField.text,
+			!emailTextField.isEmpty,
+			let passwordTextField = passwordTextField.text,
+			!passwordTextField.isEmpty,
+			let confirmPWTextField = confirmPWTextField.text,
+			!confirmPWTextField.isEmpty,
+			let pinTextField = pinTextField.text,
+			!pinTextField.isEmpty
+			else { return }
+
+		// Just for testing
+		print(name)
+		print(emailTextField)
+		print(passwordTextField)
+		print(pinTextField)
+
+	}
+
 
     /*
     // MARK: - Navigation
