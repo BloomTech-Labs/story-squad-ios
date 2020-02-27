f17710fe09d4cc19591963722722d8a6b5546966
jonalynn masters <jonalynna.masters@gmail.com>
2020-02-26 18:24:22 -0800

    WIP: Added CRUD method functions (empty functions)

--- Story Squad/Story Squad/Networking/NetworkingController.swift
+++ Story Squad/Story Squad/Networking/NetworkingController.swift
@@ -16,11 +16,40 @@ class NetworkingController {
 
     // MARK: - Parent CRUD Methods
     
+    // MARK: - Create Parent
     func createParent (name: String, email: String, password: String, pin: Int16, context: NSManagedObjectContext) {
         let id = Int16.random(in: 1..<1000)
         let parent = Parent(name: name, id: id, email: email, password: password, pin: pin, context: CoreDataStack.shared.mainContext)
         CoreDataStack.shared.save(context: context)
     }
     
+    // MARK: - Update Parent
+    func updateParent () {
+        
+    }
+    
+    // MARK: - Delete Parent
+    func deleteParent () {
+        
+    }
+    
+    // MARK: - Create Child
+    func createChild (username: String, grade: String, dyslexiaPreference: Bool, pin: Int16, avatar: Data, context: NSManagedObjectContext) {
+        let id = Int16.random(in: 1..<1000)
+        let parent = Parent()
+        //let child = Child(name: name, id: id, username: username, parent: parent, pin: pin, grade: grade, cohort: cohort, dyslexiaPreference: dyslexiaPreference, context: CoreDataStack.shared.mainContext)
+        CoreDataStack.shared.save(context: context)
+        
+    }
+    
+    // MARK: - Update Child
+    func updateChild() {
+        
+    }
+    
+    // MARK: - Delete Child
+    func deleteChild() {
+        
+    }
     
 }
