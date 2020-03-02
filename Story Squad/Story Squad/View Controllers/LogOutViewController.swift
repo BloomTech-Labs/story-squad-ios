14dc7bb305522f53d128541259833f7683043c25
Percival N <percival.ngan@gmail.com>
2020-03-02 17:08:39 -0600

    Added tab bar and refactored with storyboard references.

---
+++ Story Squad/Story Squad/View Controllers/LogOutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogOutViewController.swift
+//  Story Squad
+//
+//  Created by Percy Ngan on 3/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LogOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
