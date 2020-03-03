9bcc1e5ff3da98efeb78a1791b8c306f8fdebbe9
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-02 16:29:56 -0800

    Fixed initial tab bar to be the Dashboard

---
+++ Story Squad/Story Squad/View Controllers/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 3/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
