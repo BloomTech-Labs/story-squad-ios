9b794f18c784ee9a9d704d7da3cb8bbc47870226
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-03 19:41:00 -0800

    added carousel feature code

--- Story Squad/Story Squad/View Controllers/FamilySettingsViewController.swift
+++ Story Squad/Story Squad/View Controllers/FamilySettingsViewController.swift
@@ -6,4 +6,68 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
+import UIKit
+import ScalingCarousel
 
+class Cell: ScalingCarouselCell {}
+
+class FamilySettingsViewController: UIViewController {
+    @IBOutlet weak var carousel: ScalingCarouselView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        carousel.deviceRotated()
+    }
+    
+}
+
+typealias CarouselDatasource = FamilySettingsViewController
+extension CarouselDatasource: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "childCollectionViewCell", for: indexPath)
+        
+        if let scalingCell = cell as? ScalingCarouselCell {
+            scalingCell.mainView.backgroundColor = .white
+        }
+
+        DispatchQueue.main.async {
+            cell.setNeedsLayout()
+            cell.layoutIfNeeded()
+        }
+        
+        return cell
+    }
+}
+
+typealias CarouselDelegate = FamilySettingsViewController
+extension FamilySettingsViewController: UICollectionViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        carousel.didScroll()
+        
+       // guard let currentCenterIndex = carousel.currentCenterCellIndex?.row else { return }
+    }
+}
+
+private typealias ScalingCarouselFlowDelegate = FamilySettingsViewController
+extension ScalingCarouselFlowDelegate: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
+}
