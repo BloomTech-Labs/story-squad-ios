b81019c16ae738db641dabb6954c5ead26960fb2
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-10 12:25:48 -0700

    Added helper file for Random Avatar Added arrayOfAvatars

---
+++ Story Squad/Story Squad/Helper/RandomAvatarGenerator.swift
@@ -0,0 +1,13 @@
+//
+//  RandomAvatarGenerator.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 3/10/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+var arrayOfAvatars = ["Hero 6.png, Hero 11.png, Hero 12.png, Hero 13.png, Hero 14.png, Hero 15.png, Hero 16.png, Hero 18.png, Hero 19.png"]
+
+let randomAvatar = arrayOfAvatars.randomElement()
