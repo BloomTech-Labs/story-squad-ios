8150f4472cf5e761b109d9ac3d645b2d00fd7898
kima-92 <45408202+kima-92@users.noreply.github.com>
2020-03-10 12:38:54 -0400

    Revert "Revert "Analytics""

--- Story Squad/Story Squad/View Controllers/MainTabBarController.swift
+++ Story Squad/Story Squad/View Controllers/MainTabBarController.swift
@@ -19,6 +19,17 @@ class MainTabBarController: UITabBarController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Adds Google Analytics tracking for this view controller
+        guard let tracker = GAI.sharedInstance().defaultTracker else { return }
+        tracker.set(kGAIScreenName, value: "MainTabBarController")
+
+        guard let builder = GAIDictionaryBuilder.createScreenView() else { return }
+        tracker.send(builder.build() as [NSObject : AnyObject])
+    }
+    
 
     /*
     // MARK: - Navigation
