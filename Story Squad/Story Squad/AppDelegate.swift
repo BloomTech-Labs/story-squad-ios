2c183215938674333ac8bd1f4dfcd4f5cf49201c
kima-92 <45408202+kima-92@users.noreply.github.com>
2020-03-10 11:52:39 -0400

    Revert "Analytics"

--- Story Squad/Story Squad/AppDelegate.swift
+++ Story Squad/Story Squad/AppDelegate.swift
@@ -7,24 +7,12 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-        
-       guard let gai = GAI.sharedInstance() else {
-          assert(false, "Google Analytics not configured correctly")
-        }
-        gai.tracker(withTrackingId: "YOUR_TRACKING_ID")
-        // Optional: automatically report uncaught exceptions.
-        gai.trackUncaughtExceptions = true
-
-        // Optional: set Logger to VERBOSE for debug information.
-        // Remove before app release.
-        gai.logger.logLevel = .verbose;
+        // Override point for customization after application launch.
         return true
     }
 
