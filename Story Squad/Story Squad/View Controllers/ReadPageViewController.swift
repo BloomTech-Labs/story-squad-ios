b044829685c9072e149da4388fe98db2df1a67c1
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-18 23:25:29 -0700

    Added PageViewController and some mock ViewControllers with text

---
+++ Story Squad/Story Squad/View Controllers/ReadPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReadPageViewController.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ReadPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
