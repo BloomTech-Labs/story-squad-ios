02d7685d2b09192ec71e3a44ca539ba46b02f6eb
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-18 21:28:32 -0700

    Implemented Accept Mission Dashboard; Added Storyboard References for Read, Write, Draw; Added CollectionViewCell files

---
+++ Story Squad/Story Squad/Views/AcceptMissionCells/DrawCell.swift
@@ -0,0 +1,13 @@
+//
+//  DrawCell.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class DrawCell: UICollectionViewCell {
+    
+}
---
+++ Story Squad/Story Squad/Views/AcceptMissionCells/WriteCell.swift
@@ -0,0 +1,13 @@
+//
+//  WriteCell.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class WriteCell: UICollectionViewCell {
+    
+}
