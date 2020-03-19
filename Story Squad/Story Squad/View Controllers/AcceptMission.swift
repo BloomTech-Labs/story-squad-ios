07021f2347276ca914620c4162cc89e46892a667
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-18 22:05:00 -0700

    added VC to new storyboards to satisfy reference

--- Story Squad/Story Squad/View Controllers/AcceptMission.swift
+++ Story Squad/Story Squad/View Controllers/AcceptMission.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
 class AcceptMission: UIViewController {
-
+   
+    // MARK: - Properties
+    
+    var networkingController: NetworkingController?
+    var parentUser: Parent?
+    var childUser: Child?
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var acceptMissionCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       updateViews()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private func updateViews() {
+        acceptMissionCollectionView.delegate = self
+        acceptMissionCollectionView.dataSource = self
+    }
 
     /*
     // MARK: - Navigation
@@ -27,4 +46,38 @@ class AcceptMission: UIViewController {
     }
     */
 
+}
+
+extension AcceptMission: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, NSFetchedResultsControllerDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if indexPath.item == 0 {
+           //swiftlint:disable force_cast
+        let acceptMissionCell = acceptMissionCollectionView.dequeueReusableCell(withReuseIdentifier: "ReadCell", for: indexPath) as! ReadCell
+        
+        return acceptMissionCell
+            
+        } else if indexPath.item == 1 {
+           //swiftlint:disable force_cast
+        let adventurePassportCell = acceptMissionCollectionView.dequeueReusableCell(withReuseIdentifier: "WriteCell", for: indexPath) as! WriteCell
+           
+        return adventurePassportCell
+            
+        } else {
+           //swiftlint:disable force_cast
+        let trophyRoomCell = acceptMissionCollectionView.dequeueReusableCell(withReuseIdentifier: "DrawCell", for: indexPath) as! DrawCell
+           
+        return trophyRoomCell
+            
+        }
+    }
+        //swiftlint:disable force_cast
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    //performSegue(withIdentifier: "ChildProfileFromDashboardSegue", sender: self)
+    }
 }
