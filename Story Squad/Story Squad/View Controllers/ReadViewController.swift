819308f051b10e0be1302f1465cf8e064a81df11
jonalynn masters <jonalynna.masters@gmail.com>
2020-03-19 18:27:53 -0700

    Implementing PDFKit initial commit

---
+++ Story Squad/Story Squad/View Controllers/ReadViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PDFViewController.swift
+//  Story Squad
+//
+//  Created by Jonalynn Masters on 3/19/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import PDFKit
+
+class ReadViewController: UIViewController {
+
+    @IBOutlet weak var pdfView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
