91a814105113bd5178478c228acf480b5ef06351
Wilma <kima_92@fastmail.com>
2020-03-26 09:12:44 -0400

    Functionality implemented but not used yet for getting the pdf of the Story from the server

--- Story Squad/Story Squad/Networking/NetworkingController.swift
+++ Story Squad/Story Squad/Networking/NetworkingController.swift
@@ -639,6 +639,53 @@ class NetworkingController {
         }.resume()
     }
     
+    // MARK: - Get Story
+    func getStoryFromServer(child: Child, completion: @escaping(Result<String?, NetworkingError>) -> Void) {
+        
+        guard let childBearer = childBearer else {
+            completion(Result.failure(NetworkingError.noBearer))
+            return
+        }
+        
+        let registerURL = baseURL
+            .appendingPathComponent("canon")
+            .appendingPathComponent("1")
+        
+        var request = URLRequest(url: registerURL)
+        
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("\(childBearer.token)", forHTTPHeaderField: HeaderNames.authorization.rawValue)
+        
+        print("Request: \(request)")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                print("Error getting response: \(error)")
+                completion(.failure(.serverError(error)))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 || response.statusCode == 201 {
+                    print("Good response")
+                    completion(.success("Succesfully Submitted Story"))
+                    return
+                } else {
+                    print("Bad response, code: \(response.statusCode)")
+                    completion(.failure(.unexpectedStatusCode(response.statusCode)))
+                    return
+                }
+            }
+            
+            guard data != nil else {
+                completion(.failure(.noData))
+                return
+            }
+            // TODO: Decode the Story after a CoreData Model is created for a Story
+        }.resume()
+    }
+    
     // MARK: - Parent CRUD Methods
     
     // Create Parent
